77fb1901bf7d15c839c7e524fbd7accb67de16f2
Leonidas Yopan <leonidasyopan@gmail.com>
2020-09-02 12:28:14 -0300

    grabbing time from the API

--- lib/src/pages/loading.dart
+++ lib/src/pages/loading.dart
@@ -8,16 +8,36 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
+  void getTime() async {
     Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
-    Map decodedResponse = jsonDecode(response.body);
+        await get('http://worldtimeapi.org/api/timezone/America/Sao_Paulo');
+
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'];
+    String offsetDirection = offset.substring(0, 1);
+    print(dateTime);
+    print(offset);
+    print(offsetDirection);
+
+    offset = offset.substring(1, 3);
+
+    DateTime now = DateTime.parse(dateTime);
+    // print(now);
+    if (offsetDirection == '+') {
+      now = now.add(new Duration(hours: int.parse(offset)));
+    } else {
+      now = now.subtract(new Duration(hours: int.parse(offset)));
+    }
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
