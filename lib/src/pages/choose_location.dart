e6a8b47709c0f5bc3adafcb421f392fc33796a06
Leonidas Yopan <leonidasyopan@gmail.com>
2020-09-02 17:55:00 -0300

    Completed the Choose Location page

--- lib/src/pages/choose_location.dart
+++ lib/src/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:time_in_the_world/src/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,12 +7,60 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    void updateTime(index) async {
+      WorldTime instance = locations[index];
+      await instance.getTime();
+
+      Navigator.pop(context, {
+        'location': instance.location,
+        'flag': instance.flag,
+        'time': instance.time,
+        'isDaytime': instance.isDaytime,
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Choose Location'),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 2.0),
+            child: Card(
+              child: ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 3.0),
+                leading: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('assets/flags/${locations[index].flag}'),
+                ),
+                title: Text(locations[index].location),
+                onTap: () {
+                  setState(() {
+                    updateTime(index);
+                  });
+                },
+              ),
+            ),
+          );
+        },
       ),
     );
   }
